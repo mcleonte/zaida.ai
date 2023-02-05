6c4eb3228265e1ee0252d6ac213c1e32e0fb4196
Mihail Costin Leonte <git@mcleonte.com>
2023-02-05 17:19:30 +0200

    add main function

--- src/zaida/nlu.py
+++ src/zaida/nlu.py
@@ -1,5 +1,5 @@
 """
-Natural Languange Understanding module with Rasa
+Natural Languange Understanding module with dockerized Rasa
 """
 
 import requests
@@ -30,3 +30,12 @@ class NLU:
       return resp.json()[0]["text"]
     else:
       return f"Sorry, Rasa server returned status code {resp.status_code}"
+
+
+def main():
+  nlu = NLU()
+  print(nlu.interpret("what time is it"))
+
+
+if __name__ == "__main__":
+  main()
