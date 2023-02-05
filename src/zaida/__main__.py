1f09773a55981a2bcf766e82cece33a883ab53b6
Mihail Costin Leonte <git@mcleonte.com>
2023-02-05 15:46:34 +0200

    add nlu integration

--- src/zaida/__main__.py
+++ src/zaida/__main__.py
@@ -1,12 +1,20 @@
+"""
+Zaida AI assistant entrypoint
+"""
+
 from zaida.tts import TTS
 from zaida.stt import STT
+from zaida.nlu import NLU
 
 tts = TTS()
 stt = STT()
+nlu = NLU()
 
 for text, is_final in stt.listen():
   if is_final:
     print("\n", text, sep="")
-    tts.say(text)
+    resp = nlu.interpret(text)
+    print(resp)
+    tts.say(resp)
   else:
     print(f"partial: {text}\r", end="")
