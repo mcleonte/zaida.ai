d4e4a4329be615aca9a1ca97ebdb53f01243f422
Mihail Costin Leonte <git@mcleonte.com>
2023-02-05 15:45:43 +0200

    init commit

---
+++ src/zaida/nlu.py
@@ -0,0 +1,30 @@
+import requests
+import subprocess
+import pathlib
+import json
+
+
+class NLU:
+
+  def __init__(
+      self,
+      uri=None,
+      protocol="http",
+      hostname="localhost",
+      port=5005,
+  ):
+
+    if uri is None:
+      uri = f"{protocol}://{hostname}:{port}/webhooks/rest/webhook"
+    self.uri = uri
+
+  def interpret(self, text):
+    resp = requests.post(
+        self.uri,
+        data=json.dumps({"message": text}),
+        timeout=None,
+    )
+    if resp.ok:
+      return resp.json()[0]["text"]
+    else:
+      return f"Sorry, Rasa server returned status code {resp.status_code}"
