0e1ba856e91a806db1eae58fe82c22e2f7c5cebd
Mihail Costin Leonte <git@mcleonte.com>
2023-02-09 23:22:40 +0200

    simplified variable name

--- build/docker/stt/get-vosk-model.sh
+++ build/docker/stt/get-vosk-model.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-stt_vosk_model="vosk-model-en-us-0.42-gigaspeech"
-echo "Downloading Speech-to-Text Vosk model: $stt_vosk_model"
+vosk_model="vosk-model-en-us-0.42-gigaspeech"
+echo "Downloading Vosk model: $vosk_model"
 curl \
-  https://alphacephei.com/vosk/models/$stt_vosk_model.zip \
-  --output $stt_vosk_model.zip
-unzip $stt_vosk_model.zip
-mv $stt_vosk_model res/stt_vosk_model
-rm $stt_vosk_model.zip
+  https://alphacephei.com/vosk/models/$vosk_model.zip \
+  --output $vosk_model.zip
+unzip $vosk_model.zip
+rm $vosk_model.zip
+mv $vosk_model model
