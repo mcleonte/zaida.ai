c3fe1788a3043c37a83c2c22c8bd3ce2de3b4df5
Mihail Costin Leonte <git@mcleonte.com>
2023-02-10 00:34:28 +0200

    new main class and server classes

--- src/zaida/__main__.py
+++ src/zaida/__main__.py
@@ -1,20 +1,33 @@
 """
 Zaida AI assistant entrypoint
 """
+from zaida.nlu import NLUserver
+from zaida.tts import TTSserver
+from zaida.stt import STTserver
+import asyncio
 
-from zaida.tts import TTS
-from zaida.stt import STT
-from zaida.nlu import NLU
 
-tts = TTS()
-stt = STT()
-nlu = NLU()
+class Zaida:
+  """ Zaida AI main class """
 
-for text, is_final in stt.listen():
-  if is_final:
-    print("\n", text, sep="")
-    resp = nlu.interpret(text)
-    print(resp)
-    tts.say(resp)
-  else:
-    print(f"partial: {text}\r", end="")
+  def __init__(self):
+    self.nlu = NLUserver("http://localhost:5005")
+    self.stt = STTserver("ws://localhost:2700")
+    self.tts = TTSserver()
+
+  async def listen(self):
+    async for text, is_partial in self.stt.listen():
+      if is_partial:
+        print(f"partial: {text}\r", end="")
+        continue
+      elif text:
+        print("\n", text, sep="")
+        resp = self.nlu.interpret(text)
+        print(resp)
+        self.tts.say(resp)
+
+  def run(self):
+    asyncio.run(self.listen())
+
+
+Zaida().run()
