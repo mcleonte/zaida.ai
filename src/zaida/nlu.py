7e0e8429ffd781183f6889d94e4fd50760292e40
Mihail Costin Leonte <git@mcleonte.com>
2023-02-14 22:32:42 +0200

    rename to NLUserver; minor changes

--- src/zaida/nlu.py
+++ src/zaida/nlu.py
@@ -4,11 +4,12 @@ Natural Languange Understanding module with dockerized Rasa
 
 import requests
 import json
+from pprint import pprint
 
 
-class NLU:
+class NLUserver:
   """
-  Interface class for communicatind with Rasa server container.
+  Interface class for communicating with Rasa server.
   """
 
   def __init__(
@@ -20,7 +21,8 @@ class NLU:
   ):
 
     if uri is None:
-      uri = f"{protocol}://{hostname}:{port}/webhooks/rest/webhook"
+      uri = f"{protocol}://{hostname}:{port}"
+    uri += "/webhooks/rest/webhook"
     self.uri = uri
 
   def interpret(self, text):
@@ -29,16 +31,20 @@ class NLU:
         data=json.dumps({"message": text}),
         timeout=None,
     )
-    if resp.ok:
-      return resp.json()[0]["text"]
-    else:
+    if not resp.ok:
       return f"Sorry, Rasa server returned status code {resp.status_code}"
+    try:
+      return resp.json()[0]["text"]
+    except IndexError as ie:
+      raise SyntaxError(f"Rasa returned empty response: {resp.json()}") from ie
 
 
-def main():
-  nlu = NLU()
-  print(nlu.interpret("What time is it in Amsterdam?"))
+def main(text=None):
+  nlu = NLUserver()
+  text = text or "What time is it in Amsterdam?"
+  print(nlu.interpret(text))
 
 
 if __name__ == "__main__":
-  main()
+  import sys
+  main(sys.argv[-1])
