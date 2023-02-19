7752358d743442e244c7e3e2a412f21deb64bffd
Mihail Costin Leonte <git@mcleonte.com>
2023-02-19 21:34:21 +0200

    add logger and pre-transcribe steps

--- build/docker/stt/serve.py
+++ build/docker/stt/serve.py
@@ -1,33 +1,40 @@
-import websockets
-import asyncio
-import whisper
+"""
+Speech to text websocket server module
+"""
+
 import logging
+import asyncio
+import websockets
 import numpy as np
+import torch
+import whisper
 
 PORT = 8765
+MODEL = "small.en"
+FP16 = torch.cuda.is_available()
 
 logger = logging.getLogger("STTserver")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-logger.info("Model loading...")
-model = whisper.load_model("small.en")
+logger.info("Loading model '%s'", MODEL)
+model = whisper.load_model(MODEL)
 logger.info("Model loaded.")
 
 
-async def transcribe(websocket, path=None):
+async def transcribe(websocket):
   async for audio in websocket:
-    audio = np.frombuffer(audio, dtype=np.int16)
-    result = model.transcribe(audio)
-    logger.info(result)
-    await websocket.send(result)
+    logger.debug("Received audio chunk of size %s", len(audio))
+    audio = np.frombuffer(audio, np.int16).flatten().astype(
+        np.float32) / 32768.0
+    result = model.transcribe(audio, fp16=FP16)
+    logger.debug("Result: '%s'", result)
+    await websocket.send(result["text"])
 
 
 async def main():
-  async with websockets.serve(transcribe, "0.0.0.0", PORT):
-    logger.info(f"STTserver listening on port {PORT}")
+  async with websockets.serve(transcribe, "0.0.0.0", PORT, logger=logger):
     await asyncio.Future()
 
 
-# if __name__ == "__main__":
 asyncio.run(main())
