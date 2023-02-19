2e4f587ae472b5a7b5bdf2f8fead83cfab9340ae
Mihail Costin Leonte <git@mcleonte.com>
2023-02-20 01:35:46 +0200

    migrate from Vosk to OpenAI Whisper

--- build/docker/stt/get-vosk-model.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-vosk_model="vosk-model-en-us-0.42-gigaspeech"
-echo "Downloading Vosk model: $vosk_model"
-curl \
-  https://alphacephei.com/vosk/models/$vosk_model.zip \
-  --output $vosk_model.zip
-unzip $vosk_model.zip
-rm $vosk_model.zip
-mv $vosk_model model
