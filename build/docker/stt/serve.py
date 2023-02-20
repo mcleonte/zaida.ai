2937b2b764a832aaa575ba91302c64e9d15c5f21
Mihail Costin Leonte <git@mcleonte.com>
2023-02-20 02:48:17 +0200

    add env vars for stt model name and path

--- build/docker/stt/serve.py
+++ build/docker/stt/serve.py
@@ -8,27 +8,31 @@ import websockets
 import numpy as np
 import torch
 import whisper
+import os
 
-PORT = 8765
-MODEL = "small.en"
+PORT = os.environ["PORT"] or 8765
+MODEL_NAME = os.environ["MODEL_NAME"] or "small.en"
+MODEL_PATH = os.environ["MODEL_PATH"] or "/app/models/"
 FP16 = torch.cuda.is_available()
 
 logger = logging.getLogger("STTserver")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-logger.info("Loading model '%s'", MODEL)
-model = whisper.load_model(MODEL)
+logger.info("Loading model '%s'", MODEL_NAME)
+model = whisper.load_model(MODEL_NAME, download_root=MODEL_PATH)
 logger.info("Model loaded.")
 
 
 async def transcribe(websocket):
   async for audio in websocket:
-    logger.debug("Received audio chunk of size %s", len(audio))
+    logger.info("Received audio chunk of size %s", len(audio))
     audio = np.frombuffer(audio, np.int16).flatten().astype(
         np.float32) / 32768.0
     result = model.transcribe(audio, fp16=FP16)
-    logger.debug("Result: '%s'", result)
+    logger.info("Result: '%s'", result)
+    for i, segment in enumerate(result["segments"]):
+      logger.info("Segment %s: '%s'", i, segment)
     await websocket.send(result["text"])
 
 
