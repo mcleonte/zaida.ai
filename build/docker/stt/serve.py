4f2664517c9ef3340b118f43024ea0b35fe67636
Mihail Costin Leonte <git@mcleonte.com>
2023-03-03 23:51:10 +0200

    major architectural changes, unify client class, setup direct traffic between server containers

--- build/docker/stt/serve.py
+++ build/docker/stt/serve.py
@@ -2,10 +2,9 @@
 Speech to text websocket server module
 """
 
+import os
 import logging
 import asyncio
-import socketio
-import os
 import requests
 import json
 
@@ -17,13 +16,13 @@ import whisper
 PROXY_PORT = os.environ["PROXY_PORT"]
 NLU_HOST = os.environ["NLU_HOST"]
 NLU_PORT = os.environ["NLU_PORT"]
-NLU_URI = f"http://{NLU_HOST}:{NLU_PORT}/socket.io"
-# NLU_URI = f"http://{NLU_HOST}:{NLU_PORT}/webhooks/rest/webhook"
+NLU_URI = f"http://{NLU_HOST}:{NLU_PORT}/webhooks/callback/webhook"
 
 MODEL_NAME = os.environ["MODEL_NAME"] or "small.en"
 MODEL_PATH = os.environ["MODEL_PATH"] or "/app/models/"
 FP16 = torch.cuda.is_available()
 
+logging.basicConfig(format="%(asctime)s | %(message)s")
 logger = logging.getLogger("zaida.stt")
 logger.setLevel(os.environ["LOG_LEVEL"])
 logger.addHandler(logging.StreamHandler())
@@ -31,8 +30,6 @@ logger.addHandler(logging.StreamHandler())
 logger.info("Loading model '%s'", MODEL_NAME)
 model = whisper.load_model(MODEL_NAME, download_root=MODEL_PATH)
 logger.info("Model loaded.")
-
-queue = asyncio.Queue()
 
 
 def transcribe(audio):
@@ -42,55 +39,30 @@ def transcribe(audio):
   logger.info("Result: '%s'", result)
   for i, segment in enumerate(result["segments"]):
     logger.info("Segment %s: '%s'", i, segment)
-  return result["text"]
+  return result
 
 
-async def serve_forever():
-
-  async def serve(websocket):
-    async for audio in websocket:
-      queue.put_nowait(transcribe(audio))
-
-  async with websockets.serve(serve, "0.0.0.0", PROXY_PORT, logger=logger):
-    await asyncio.Future()
-
-
-async def send_forever():
-  sio = socketio.AsyncClient(logger=True)
-
-  @sio.event
-  async def connect():
-    logger.info("STT Connected to NLU")
-
-  await sio.connect(NLU_URI)
-  while True:
-    await sio.emit(
-        "message",
-        {
-            "sender": "zaida-stt",
-            "message": await queue.get(),
-            # "metadata": json.dumps({})
-        },
-    )
+def send(text: str):
+  logger.debug("Sending text to %s: %s", NLU_URI, text)
+  requests.post(
+      NLU_URI,
+      json.dumps({
+          "message": text,
+          "sender": "mcleonte",
+      }),
+      stream=True,
+      timeout=None,
+  )
 
 
-# async def send_forever():
-#   async with websockets.connect(NLU_URI, logger=logger) as websocket:
-#     while True:
-#       await websocket.send(await queue.get())
-#     # requests.post(
-#     #     NLU_URI,
-#     #     data=json.dumps({"message": result["text"]}),
-#     #     stream=True,
-#     #     timeout=None,
-#     # )
+async def serve(websocket):
+  async for audio in websocket:
+    send(transcribe(audio)["text"])
 
 
 async def main():
-  await asyncio.gather(
-      serve_forever(),
-      send_forever(),
-  )
+  async with websockets.serve(serve, "0.0.0.0", PROXY_PORT, logger=logger):
+    await asyncio.Future()
 
 
 asyncio.run(main())
--- src/zaida/main.py
+++ src/zaida/main.py
@@ -1,35 +1,150 @@
 """
-Zaida AI assistant entrypoint
+Zaida AI client entrypoint
 """
 
 import asyncio
+import websockets
 import logging
+from ctypes import CFUNCTYPE, cdll, c_int, c_char_p
 
-from zaida.nlu import NLUclient
-from zaida.tts import TTSclient
-from zaida.stt import STTclient
+import speech_recognition as sr
+import sounddevice as sd
 
-logger = logging.getLogger("zaida")
-logger.setLevel(logging.INFO)
+logging.basicConfig(format="%(asctime)s | %(message)s")
+logger = logging.getLogger("zaida.client")
+logger.setLevel(logging.DEBUG)
+
+# Supress ALSA warnings and errors from terminal.
+# https://stackoverflow.com/questions/7088672#answer-13453192
 
 
-class Zaida:
-  """ Zaida AI main class """
+# pylint: disable=unused-argument
+def py_error_handler(filename, line, function, err, fmt):
+  pass
 
-  def __init__(self):
-    self.nlu = NLUclient("http://localhost:5005")
-    self.stt = STTclient("ws://localhost:8765")
-    self.tts = TTSclient("http://localhost:59125/api/tts")
 
-  async def listen(self):
-    async for text in self.stt.listen():
-      logging.debug(text)
-      resp = self.nlu.interpret(text)
-      logging.debug(resp)
-      self.tts.say(resp)
+make_cfunc = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+c_error_handler = make_cfunc(py_error_handler)
+asound = cdll.LoadLibrary("libasound.so")
+asound.snd_lib_error_set_handler(c_error_handler)
+
+CLIENT_PORT = 8088
+
+
+class ZaidaClient:
+  """
+  Client class for communicating with the STT websocket server.
+  """
+
+  def __init__(
+      self,
+      uri=None,
+      protocol="ws",
+      hostname="localhost",
+      nlu_port=8000,
+      stt_port=8080,
+      tts_port=8088,
+      energy_threshold=150,
+      output_device="pipewire",
+  ):
+
+    if uri is None:
+      uri = f"{protocol}://{hostname}"
+    self.nlu_uri = f"{uri}:{nlu_port}"
+    self.stt_uri = f"{uri}:{stt_port}"
+    self.tts_uri = f"{uri}:{tts_port}"
+
+    self.configure_output_stream(output_device)
+    self.configure_input_stream(energy_threshold)
+
+  def configure_input_stream(self, energy_threshold):
+
+    self.mic = sr.Microphone(sample_rate=16000)
+    self.rec = sr.Recognizer()
+    self.rec.pause_threshold = 1
+    self.queue = asyncio.Queue()
+
+    if energy_threshold is None:
+      self.calibrate()
+    else:
+      self.rec.energy_threshold = energy_threshold
+      self.rec.dynamic_energy_threshold = False
+
+  def configure_output_stream(self, output_device):
+    self.output_device = sd.query_devices(output_device, "output")
+    self.output_device_index = self.output_device["index"]
+
+    # settings supported by mimic3
+    self.output_channels = 1
+    self.bytes_per_sample = 2  # 16 bits
+    self.samples_per_second = 22050
+
+    frame_duration = 10
+    self.samples_per_frame = (frame_duration * self.samples_per_second) // 1000
+
+    self.stream = sd.RawOutputStream(
+        samplerate=self.samples_per_second,
+        blocksize=self.samples_per_frame,
+        device=self.output_device_index,
+        channels=self.output_channels,
+        dtype="int16",
+    )
+
+  def calibrate(self, duration: float = 3.):
+    with self.mic as source:
+      logger.info("Calibrating microphone...")
+      self.rec.adjust_for_ambient_noise(source, duration=duration)
+      logger.info("Microphone calibrated to energy threshold of %s",
+                  self.rec.energy_threshold)
+
+  def say(self, bytestring: bytes):
+    self.stream.start()
+    self.stream.write(bytestring)
+    self.stream.stop()
+
+  async def listen_forever(self):
+
+    loop = asyncio.get_running_loop()
+
+    def callback(recognizer: sr.Recognizer, audio: sr.AudioData):
+      logger.info("Callback called, putting audio of size %s in queue",
+                  len(raw_data := audio.get_raw_data()))
+      loop.call_soon_threadsafe(self.queue.put_nowait, raw_data)
+
+    async with websockets.connect(self.stt_uri, logger=logger) as websocket:
+      self.stop_listening = self.rec.listen_in_background(self.mic, callback)
+      try:
+        while True:
+          logger.debug("Waiting for audio queue...")
+          await websocket.send(await self.queue.get())
+          logger.debug("Audio sent to server.")
+      except KeyboardInterrupt:
+        self.stop_listening(wait_for_stop=False)
+
+  async def answer_forever(self):
+
+    self.stream.start()
+    try:
+      async with websockets.connect(self.tts_uri, logger=logger) as websocket:
+        async for bytestring in websocket:
+          self.stream.write(bytestring)
+    except KeyboardInterrupt:
+      self.stream.stop()
+
+  async def communicate(self):
+    await asyncio.gather(
+        self.listen_forever(),
+        self.answer_forever(),
+    )
 
   def run(self):
-    asyncio.run(self.listen())
+    asyncio.run(self.communicate())
 
 
-Zaida().run()
+def main():
+  client = ZaidaClient()
+  client.run()
+
+
+if __name__ == "__main__":
+  main()
