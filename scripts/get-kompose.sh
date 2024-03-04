b7836776125b175610a3c147e2979a93d6dce10c
Mihail Costin Leonte <git@mcleonte.com>
2024-03-04 17:24:53 +0200

    rm kompose implementation

--- scripts/get-kompose.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-# https://kubernetes.io/docs/tasks/configure-pod-container/translate-compose-kubernetes/#install-kompose
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-linux-amd64 -o kompose
