81fdf8e0519d4734903ea5169a5c9bbddc369c65
Mihail Costin Leonte <git@mcleonte.com>
2024-03-06 06:26:07 +0200

    parametrize startup cmd

--- docker/tts/run.sh
+++ docker/tts/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 /home/mimic3/app/.venv/bin/python3 app.py & \
-/home/mimic3/app/.venv/bin/python3 -m mimic3_http --cuda & \
+/home/mimic3/app/.venv/bin/python3 -m mimic3_http \
+  --preload-voice en_US/vctk_low \
+  --voice ${VOICE} \
+  --debug \
+  --port ${PORT_1} \
+  --cuda & \
 wait
 
 
